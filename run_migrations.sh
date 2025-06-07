962f69cbbb729dc04c6c33228670bfe782528bdf
stick35em10 <stick35em10@cloudshell>
2025-06-07 13:54:28 +0000

    update DEBUG = True

---
+++ run_migrations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+echo "--- Installing Python dependencies ---"
+pip install -r requirements.txt
+
+echo "--- Collecting static files ---"
+python manage.py collectstatic --no-input
+
+echo "--- Applying database migrations ---"
+python manage.py migrate
+
+echo "--- Build process completed successfully! ---"
+
+#python3 manage.py migrate
