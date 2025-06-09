b1d009a92b0f849081c3e617553f01c94dc714c3
stick35em10 <stick35em10@cloudshell>
2025-06-09 09:23:57 +0000

    5. Registrar os Models no Admin

--- projects_management/admin.py
+++ projects_management/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
-
+#from django.contrib import admin
 # Register your models here.
+# projects_management/admin.py
+
+from django.contrib import admin
+from .models import Status, Project
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    list_filter = ('name',) # Permite filtrar por status no admin
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'start_date')
+    list_filter = ('status', 'start_date') # Permite filtrar por status e data
+    search_fields = ('name',) # Permite pesquisar por nome do projeto
+    raw_id_fields = ('status',) # Opcional: para selects grandes de Status, mostra um ID em vez de um dropdown
