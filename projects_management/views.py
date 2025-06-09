929f4b73c5124ca96ee7c3e472ad5a1fbe8841f6
stick35em10 <stick35em10@cloudshell>
2025-06-09 10:52:28 +0000

    import Project, ProjectoForm and  create functions lista_projetos, criar_projecto, atualizar_status

--- projects_management/views.py
+++ projects_management/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project
+from .forms import ProjectoForm
+
+def lista_projectos(request):
+    projectos = Project.objects.all()
+    # projectos/lista.html done in 8. (Opcional) Crie templates
+    return render(request, 'projectos/lista.html', {'projectos': projectos})
+
+def criar_projecto(request):
+    if request.method == 'POST':
+        form = ProjectoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_projectos')
+    else:
+        form = ProjectoForm()
+        # must do in 8. (Opcional) Crie templates
+    return render(request, 'projectos/form.html', {'form': form})
+
+def atualizar_status(request, pk, novo_status):
+    projecto = get_object_or_404(Project, pk=pk)
+    projecto.status = novo_status
+    projecto.save()
+    return redirect('lista_projectos')
